a9643127a23ef4c15e4ebd486bc44c818ded8ec9
Bayovrosky <aoluwatosin00@gmail.com>
2023-03-16 17:31:19 -0700

    Betty rule 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,10 @@
 #include<stdio.h>
-
 /**
- *
- *  * main - Entry point
- *
+ *  * main - entry point
  *   *
- *
- *    * Return: Always 0 (Success)
- *
+ *    * return: always 0 (success)
  *     */
 int main(void)
-
 {
-
-	        puts("\"Programming is like building a multilingual puzzle");
-
+	        puts("\"programming is like building a multilingual puzzle");
 		        return (0);
-
-}
