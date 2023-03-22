4cc761e24765e219d9ba83dc3d98caef204987fd
Bayovrosky <aoluwatosin00@gmail.com>
2023-03-21 17:49:02 -0700

    gr

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,11 +2,13 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * @n: integer value to determine the size of times table
+ *
+ * Return: void
  */
 void print_times_table(int n)
 {
-	int i, j, k;
+	int i, j, res;
 
 	if (n >= 0 && n <= 15)
 	{
@@ -14,31 +16,31 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
+				res = i * j;
+
+				if (j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
+				}
+
+				if (res < 10 && j != 0)
 				{
-					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
+					_putchar(res + '0');
+				}
+				else if (res < 100)
 				{
-					_putchar(',');
 					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					_putchar(res / 10 + '0');
+					_putchar(res % 10 + '0');
+				}
+				else
+				{
+					_putchar(res / 100 + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar(res % 10 + '0');
 				}
 			}
 			_putchar('\n');
