a247a0ee338ad896584e4f2f44ea9cda9b6efbe8
Bayovrosky <aoluwatosin00@gmail.com>
2023-04-03 17:23:10 -0700

    Meme set Function

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
- * _memset - Fills the first n bytes of the memory area pointed to by s with
- *           the constant byte b.
+ * _memset - fills the first n bytes of the memory area pointed to by s with
+ * the constant byte b
+ * @s: pointer to the memory area to be filled
+ * @b: the constant byte to fill the memory with
+ * @n: the number of bytes to be filled
  *
- * @s: A pointer to the memory area to be filled.
- * @b: The constant byte to fill the memory area with.
- * @n: The number of bytes to be filled.
- *
- * Return: A pointer to the filled memory area @s.
+ * Return: a pointer to the filled memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+    unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b;
-	}
+    for (i = 0; i < n; i++)
+    {
+        s[i] = b;
+    }
 
-	return (s);
+    return s;
 }
