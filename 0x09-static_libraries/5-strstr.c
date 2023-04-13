c4aca356615737a8324fe5670b3929081af1ebd9
Bayovrosky <aoluwatosin00@gmail.com>
2023-04-13 15:14:07 -0700

    Resolving later?

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strstr - Entry point for string
+ * @haystack: input.
+ * @needle: input.
+ * Return: Always 0 (Success) prove
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+
+	return (0);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+		
+/**
+ * char *_strcpy - copy string characters
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
