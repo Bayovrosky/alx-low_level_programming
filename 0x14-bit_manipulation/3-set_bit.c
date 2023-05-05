ff2050abd62f4e31fab20a4b2aa214b08e67b015
Bayovrosky <aoluwatosin00@gmail.com>
2023-05-04 17:02:14 -0700

    Test keygen ssh

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * set_bit - sets a bit at a given index to 1
+ * @n: pointer to the number to change always
+ * @index: index of the bit to set to 1
+ *
+ * Return: 1 for success, -1 for failure 0
+ */
